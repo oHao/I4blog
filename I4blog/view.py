7454b23889a9e178a6723e7b319940570dea90c4
oHao <oHao_995@163.com>
2017-11-28 19:16:25 +0800

    study。。。

--- I4blog/view.py
+++ I4blog/view.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+
 def hello(request):
-	context ={}
-	context['hello']='Are you FIRST_DAY_OF_WEEKly'
-	return render(request,'hello.html',context)
+    context = {}
+    context['hello'] = 'Are you FIRST_DAY_OF_WEEKly'
+    return render(request, 'hello.html', context)
